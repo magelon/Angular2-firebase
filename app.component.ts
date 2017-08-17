c6af42575fda485ac9a9be6a8d0ebaf7f688f1e8
lon <alfardproject@gmail.com>
2017-08-16 19:25:32 -0700

    Add files via upload
    Two-way data binding, component properties.

--- app.component.ts
+++ app.component.ts
@@ -1,46 +1,33 @@
 import { Component } from '@angular/core';
 
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    //css selector
+    selector: 'my-app',
+    //one way data binding in appcomponent class
+    //multi-line template strings
+    //Import the FormsModule for two waydata binding
+    template: `<h1>{{title}}</h1>
+                <h2>{{hero.name}} details</h2>
+                <div><label>id: </label>{{hero.id}}</div>
+                <div>
+                  <label>name: </label>
+                  <input [(ngModel)]="hero.name" placeholder="name">
+                </div>
+                `,
 })
-export class AppComponent {
-  title = 'My First Angular App';
-
-  user: Observable<firebase.User>;
-  items: FirebaseListObservable<any[]>;
-  msgVal: string = '';
-  
-  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-    this.items = af.list('/messages', {
-      query: {
-        limitToLast: 50
-      }
-    });
-  
-    this.user = this.afAuth.authState;
-  
-  }
-
-  login() {
-    this.afAuth.auth.signInAnonymously();
-}
-
-logout() {
-    this.afAuth.auth.signOut();
-}
-
-Send(desc: string) {
-    this.items.push({ message: desc});
-    this.msgVal = '';
-}
-
+export class AppComponent  {
+    title = 'Tour of Heroes';
+    
+    
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 
 }
 
+//The hero needs more properties. Convert the hero from a literal string to a class.
+export class Hero {
+    id: number;
+    name: string;
+}
--- app.module.ts
+++ app.module.ts
@@ -1,42 +1,14 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-
-import { AppComponent } from './app.component';
-
-import { AngularFireModule } from 'angularfire2';
-
-// New imports to update based on AngularFire2 version 4
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyBL8RiRjGsIRTHubRzR85hlRkM22-wyUB4",
-  authDomain: "shop-d7e2e.firebaseapp.com",
-  databaseURL: "https://shop-d7e2e.firebaseio.com",
-  projectId: "shop-d7e2e",
-  storageBucket: "shop-d7e2e.appspot.com",
-  messagingSenderId: "287848933518"
-}
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule {
-
-  
- }
-
+export class AppModule { }
